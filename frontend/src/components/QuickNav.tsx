1495e48471b5c95f1ef989a56e2750fa68d196f6
keihachimaru <keihachimaru@gmail.com>
2026-02-04 00:07:45 +0100

    feat: quicknav component for better ux

---
+++ frontend/src/components/QuickNav.tsx
@@ -0,0 +1,66 @@
+import '@/styles/QuickNav.css';
+import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
+
+type Props = {
+    data: string | null;
+    sections: string[]
+}
+
+const QuickNav = ({ data, sections } : Props) => {
+    if(!data) return
+    const sectionName = data.split("[")
+    const sectionDetail = sectionName.length>1?sectionName[1].slice(0,-1):""
+    const idx = sections.indexOf(data);
+
+    function goNext() {
+        const idx = (sections.indexOf(data!)+1)%sections.length
+        const next = sections[idx]
+        const id = next.split(' ').join('-').toLocaleLowerCase();
+        const element = document.getElementById(id);
+        element?.scrollIntoView({ behavior: "smooth", block: "start" })
+    }
+
+    function goPrev() {
+        const idx = (sections.indexOf(data!)-1)
+        const next = sections[idx]
+        const id = next.split(' ').join('-').toLocaleLowerCase();
+        const element = document.getElementById(id);
+
+        const offset = 20;
+        const top = element!.getBoundingClientRect().top + window.scrollY - offset;
+        window.scrollTo({ top, behavior: "smooth" });
+    }
+
+    return sectionName.length && (
+        <div className="quickNav">
+            <button onClick={() => goPrev()}>
+                <FaCaretLeft 
+                    size={32} 
+                    color={idx>0?"var(--color-accent)":"transparent"}>
+                </FaCaretLeft>
+            </button>
+            <div
+                className="section-title"
+            >
+                <div
+                    className="title"
+                >
+                    { sectionName[0] } 
+                </div>
+                <div
+                    className="detail"
+                >
+                    { sectionDetail }
+                </div>
+            </div>
+            <button onClick={() => goNext()}>
+                <FaCaretRight 
+                    size={32} 
+                    color={idx<sections.length-1?"var(--color-accent)":"transparent"}>
+                </FaCaretRight>
+            </button>
+        </div>
+    );
+};
+
+export default QuickNav;
