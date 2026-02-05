bb8210f703f8b6892f67664d2300f006666576d8
keihachimaru <keihachimaru@gmail.com>
2026-02-05 06:07:41 +0100

    feat: loading component

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,7 +1,10 @@
-const Error = () => {
+import '@/styles/Loading.css'
+
+const Loading = () => {
     return (
-        <>
-        </>
+        <div className='loading'>
+            <span className="loader"></span>
+        </div>
     )
 }
-export default Error;
+export default Loading;
