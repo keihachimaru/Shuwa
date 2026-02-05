b53d9e1e784bbecc32bb2482dd9ff13b701ff34a
keihachimaru <keihachimaru@gmail.com>
2026-02-05 10:45:13 +0100

    feat: imported images in iconcode so they get included in build

--- frontend/src/components/IconCode.tsx
+++ frontend/src/components/IconCode.tsx
@@ -1,65 +1,79 @@
 import '@/styles/IconCode.css';
+import sesameImg from '@/assets/sesame.png';
+import fishImg from '@/assets/fish.png';
+import eggImg from '@/assets/egg.png';
+import peanutImg from '@/assets/peanut.png';
+import soySauceImg from '@/assets/soy-sauce.png';
+import molluscImg from '@/assets/mollusc.png';
+import glutenImg from '@/assets/gluten.png';
+import almondImg from '@/assets/almond.png';
+import glutenFreeImg from '@/assets/gluten-free.png';
+import mustardImg from '@/assets/mustard.png';
+import crustaceanImg from '@/assets/crustacean.png';
+import dairyImg from '@/assets/dairy.png';
+import vegetarianImg from '@/assets/vegetarian.png';
+import shuwashuwaImg from '@/assets/shuwashuwa.png';
 
 const IconCode = () => {
     const images = [
         {
             name: "Sésamo",
-            src: "/public/sesame.png"
+            src: sesameImg
         },
         {
             name: "Pescado",
-            src: "/public/fish.png"
+            src: fishImg
         },
         {
             name: "Huevo",
-            src: "/public/egg.png"
+            src: eggImg
         },
         {
             name: "Cacahuetes",
-            src: "/public/peanut.png"
+            src: peanutImg
         },
         {
             name: "Soja",
-            src: "/public/soy-sauce.png"
+            src: soySauceImg
         },
         {
             name: "Moluscos",
-            src: "/public/mollusc.png"
+            src: molluscImg
         },
         {
             name: "Cacahuetes",
-            src: "/public/gluten.png"
+            src: glutenImg
         },
         {
             name: "Frutos secos",
-            src: "/public/almond.png"
+            src: almondImg
         },
         {
             name: "Sin gluten",
-            src: "/public/gluten-free.png"
+            src: glutenFreeImg
         },
         {
             name: "Mostaza",
-            src: "/public/mustard.png"
+            src: mustardImg
         },
         {
             name: "Crustáceos",
-            src: "/public/crustacean.png"
+            src: crustaceanImg
         },
         {
             name: "Lácteos",
-            src: "/public/dairy.png"
+            src: dairyImg
         },
         {
             name: "Vegetariano",
-            src: "/public/vegetarian.png"
+            src: vegetarianImg
         }
     ];
     return (
         <div className="icon-codes">
             <img 
                 className="logo"
-                src={"/public/shuwashuwa.png"}
+                src={shuwashuwaImg}
             />
             <div className="codes">
                 {images.map((i, idx) => (
